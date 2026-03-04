5dcb3eba162fde6178010395e0f64e24f5f6d57a
Huseyin2603 <Huseyin2603@users.noreply.github.com>
2026-03-04 13:44:08 +0300

    Arayüz açılmıyordu düzeltildi

--- src/main/main.ts
+++ src/main/main.ts
@@ -31,7 +31,7 @@ function createWindow() {
     mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, '..', '..', 'dist', 'renderer', 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, '..', '..', 'renderer', 'index.html'));
   }
 
   mainWindow.on('closed', () => {
